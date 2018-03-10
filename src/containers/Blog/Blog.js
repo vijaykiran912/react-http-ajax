d373fade684c2792da97589054436dae45cc4ee5
vipothamse-copart <Vijay.Pothamsetty@copart.com>
2018-03-10 16:08:05 +0530

    Restructured project to work on Router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
+import Posts from './Posts/Posts';
+import {Route} from 'react-router-dom';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+ 
 
 class Blog extends Component {
+
     render () {
         return (
             <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <header className="Blog">
+                    <nav>
+                        <ul>
+                            <li><a href='/'>HOME</a></li>
+                            <li><a href='/new-post'>New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                
+               {/* <Route path="/" render={() => <h1>HOME</h1>}/>
+               <Route exact path="/" render={() => <h1>HOME 2</h1>}/> */}
+
+               <Route path="/" exact component={Posts}/>
+                
             </div>
         );
     }
